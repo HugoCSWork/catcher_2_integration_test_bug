aa78832f27f48b61d25506dde61d6e0c415c5640
Nico Mexis <nico.mexis@kabelmail.de>
2023-10-26 20:43:48 +0200

    Should fix issues

--- integration_test/main_test.dart
+++ integration_test/main_test.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:catcher_2/catcher_2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -5,17 +7,40 @@ import 'package:integration_test_defect/main.dart';
 
 void main() {
   group('App should', () {
-    testWidgets('Show two when floating action button is pressed twice', (tester) async {
+    testWidgets('Show two when floating action button is pressed twice',
+        (tester) async {
       // Given
-      Catcher2(runAppFunction: () => const MyApp());
+      Catcher2(
+        rootWidget: const MyApp(),
+        debugConfig: Catcher2Options(
+          SilentReportMode(),
+          [ConsoleHandler()],
+          onFlutterError: FlutterError.onError,
+          onPlatformError: PlatformDispatcher.instance.onError,
+        ),
+        releaseConfig: Catcher2Options(
+          SilentReportMode(),
+          [ConsoleHandler()],
+          onFlutterError: FlutterError.onError,
+          onPlatformError: PlatformDispatcher.instance.onError,
+        ),
+        profileConfig: Catcher2Options(
+          SilentReportMode(),
+          [ConsoleHandler()],
+          onFlutterError: FlutterError.onError,
+          onPlatformError: PlatformDispatcher.instance.onError,
+        ),
+      );
 
       // When
       await tester.tap(find.byKey(const Key('increment_floating_button')));
       await tester.pumpAndSettle();
 
       // Then
-      final valueText = tester.firstWidget<Text>(find.byKey(const Key('number_value_text')));
-      expect(valueText.data, "2"); // Will fail the test as we only press it once
+      final valueText =
+          tester.firstWidget<Text>(find.byKey(const Key('number_value_text')));
+      expect(
+          valueText.data, "2"); // Will fail the test as we only press it once
     });
   });
 }
